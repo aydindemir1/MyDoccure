84288a6087469eb61a240cd6de83a6c23482eaed
aydin <aydin@AYDIN>
2026-07-01 20:07:24 +0300

    Add project files.

---
+++ Doccure.BranchService/Entities/Branch.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Doccure.BranchService.Entities
+{
+    public class Branch
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string BranchId { get; set; }
+        public string BranchName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+
+        // Yeni Alanlar
+
+        public string BranchCode { get; set; }
+        public int DoctorCount { get; set; }
+        public int MonthlyAppointmentCount { get; set; }
+        public int BedCount { get; set; }
+        public decimal Rating { get; set; }
+        public int OccupancyRate { get; set; }
+        public string ThemeColor { get; set; }
+    }
+}
---
+++ Microservices/Doccure.BranchService/Program.cs
@@ -0,0 +1,41 @@
+using Doccure.BranchService.Services;
+using Doccure.BranchService.Settings;
+using Microsoft.Extensions.Options;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddScoped<IBranchService, BranchService>();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettingsKey"));
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+    sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
