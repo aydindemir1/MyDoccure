c2bf18fd056f55c8f7c67e418aebcbbc8f142e0e
aydin <aydin@AYDIN>
2026-07-01 20:10:34 +0300

    ...

--- Doccure.BranchService/Settings/DatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Doccure.BranchService.Settings
-{
-    public class DatabaseSettings : IDatabaseSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string BranchCollectionName { get; set; }
-    }
-}
